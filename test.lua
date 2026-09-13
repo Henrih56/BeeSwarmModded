786ce080ff42caeaeab86dff0930fa15893a43b7
Nathi <henriqueonofre45@gmail.com>
2026-09-13 16:21:14 -0400

    test: minimal script

---
+++ test.lua
@@ -0,0 +1,10 @@
+-- Teste básico Delta
+game.StarterGui:SetCore("SendNotification", {
+	Title = "TESTE";
+	Text = "Script rodou!";
+	Duration = 5;
+})
+
+print("Script teste executado")
+
+-- Se isso funcionar, o problema é o tamanho do script.lua
